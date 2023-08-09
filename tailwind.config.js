f0461703d39cf0d38cde75917b04113b024f8115
arshapradita <arshapradita@gmail.com>
2023-08-09 15:11:32 +0700

    feat: add custom font style class display

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cd: ["Clash Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
